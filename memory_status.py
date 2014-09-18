a0e3f8375b259692c24818a75f0bb1414bf2e663
Cleiton Jr Mittmann <mittmannv8@gmail.com>
2014-09-18 16:13:56 -0300

    Add support to user preference and show usage percent in label

--- memory_status.py
+++ memory_status.py
@@ -5,13 +5,23 @@ import appindicator
 import gtk
 import os
 import psutil
-
+import json
 
 class MemoryIndicator:
     def __init__(self):
         # General definitions
         self.attention = False
         self.MEGABYTE_DIVISOR = 1024 * 1024
+        
+        # Set user preferences
+        self.user_conf = os.path.join(
+            os.path.expanduser('~'),
+            '.config/memory_status_indicator.conf'
+        )
+        try:
+            self.show_label = json.load(open(self.user_conf))["show_label"]
+        except:
+            self.show_label = False
         
         # Set icons
         self.icon_path = os.path.join(
@@ -27,19 +37,29 @@ class MemoryIndicator:
             self.icon_path
         )
    
-   # Define menu
+        # Define menu
         self.menu = gtk.Menu()
-
+        
+        #title
         title = gtk.MenuItem("Memory Status")
         title.set_state(gtk.STATE_INSENSITIVE)
         self.menu.append(title)
         self.menu.append(gtk.SeparatorMenuItem())
+        #info
         self.menu_info_used = gtk.MenuItem("Loading...")
         self.menu_info_used.set_state(gtk.STATE_INSENSITIVE)
         self.menu.append(self.menu_info_used)
+        #show label
+        self.menu_conf_show_label = gtk.CheckMenuItem("Show label")
+        if self.show_label:
+            self.menu_conf_show_label.set_active(True)
+        self.menu_conf_show_label.connect("toggled", self.set_show_label)
+        self.menu.append(self.menu_conf_show_label)
+        
         self.menu.show_all()
         self.mem_indicator.set_menu(self.menu)
         self.mem_indicator.set_status(appindicator.STATUS_ACTIVE)
+        
 
         self.total_memory_available = psutil.phymem_usage().total / self.MEGABYTE_DIVISOR
         
@@ -72,9 +92,24 @@ class MemoryIndicator:
             self.mem_indicator.set_status(appindicator.STATUS_ACTIVE)
             self.attention = False
 
+        # Set label if show_label is True
+        if self.show_label:
+            self.mem_indicator.set_label("%.1f%%" % memory_status.percent)
+
         # Return True for gtk.timeout loop control
         return True
 
+    def set_show_label(self, widget):
+        if self.menu_conf_show_label.get_active():
+            self.show_label = True
+            with open(self.user_conf, 'w') as conf_file:
+                conf_file.write('{"show_label": true}')
+        else:
+            self.show_label = False
+            self.mem_indicator.set_label("")
+            
+            with open(self.user_conf, 'w') as conf_file:
+                conf_file.write('{"show_label": false}')
 
 if __name__ == "__main__":
     memory_indicator = MemoryIndicator()
