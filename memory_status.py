8ecfaeec77a19e1c9f9313b7a6f22cbf4740e3e5
Cleiton Jr Mittmann <mittmannv8@gmail.com>
2014-09-18 15:30:49 -0300

    Add gtk loop and memory information method

--- memory_status.py
+++ memory_status.py
@@ -4,10 +4,14 @@
 import appindicator
 import gtk
 import os
+import psutil
 
 
 class MemoryIndicator:
     def __init__(self):
+        # General definitions
+        self.MEGABYTE_DIVISOR = 1024 * 1024
+        
         # Set icons
         self.icon_path = os.path.join(
             os.path.dirname(os.path.abspath(__file__)),
@@ -35,6 +39,31 @@ class MemoryIndicator:
         self.menu.show_all()
         self.mem_indicator.set_menu(self.menu)
         self.mem_indicator.set_status(appindicator.STATUS_ACTIVE)
+
+        self.total_memory_available = psutil.phymem_usage().total / self.MEGABYTE_DIVISOR
+        
+        # For each X seconds, the gtk loop call the get_memory_status method
+        gtk.timeout_add(2000, self.get_memory_status)
+        
+
+    def get_memory_status(self):
+        """ Method called every x times, definied by gtk.timeout_add method
+            This get memory information using psutil library and show in indicator.
+        """
+
+        memory_status = psutil.phymem_usage()
+
+        self.menu_info_used.set_label("Memory used: %dMB / %dMB (%.1f%%)" % (
+                memory_status.used / self.MEGABYTE_DIVISOR,
+                self.total_memory_available, memory_status.percent
+            )
+        )
+
+        round_percent_memory_available = int(round(memory_status.percent, -1))
+        self.mem_indicator.set_icon("%d" % round_percent_memory_available)
+
+        # Return True for gtk.timeout loop control
+        return True
 
 
 if __name__ == "__main__":
